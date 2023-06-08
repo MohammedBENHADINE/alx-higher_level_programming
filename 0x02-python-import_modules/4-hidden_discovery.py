8219b05eabe7446b3c5dd67fcedc9670a1ae93df
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-08 10:55:47 -0700

    make it more appealing

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    list = dir(hidden_4)
-    for i in range(0, len(list)):
-        if list[i][0] == '_' and list[i][1] == '_':
-            continue
-        else:
-            print(list[i])
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)

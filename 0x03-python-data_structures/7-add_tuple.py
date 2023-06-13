69e9ca38a145ead7b6794a5b1e97c00973a87215
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-12 17:15:52 -0700

    fix returning format

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,4 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a1, a2 = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
     b1, b2 = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
-    return a1 + b1, a2 + b2
+    sum_1 = a1 + b1
+    sum_2 = a2 + b2
+    return sum_1, sum_2

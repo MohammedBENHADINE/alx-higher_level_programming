7fde7f95489120af18cedca92ac1fa5f63197c1d
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-13 16:34:32 -0700

    make a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    test = []
-    for i in my_list:
-        if i % 2 is 0:
-            test = test + [True]
-        else:
-            test = test + [False]
-    return test
+    if (len(my_list) == 0):
+        return []
+    else:
+        res = []
+        for i in range(0, len(my_list)):
+            if (my_list[i] % 2):
+                res.append(False)
+            else:
+                res.append(True)
+        return res

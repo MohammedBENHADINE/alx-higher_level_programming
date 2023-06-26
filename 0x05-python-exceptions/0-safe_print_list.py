f2b9beb80597af5812eac37ee6642e0d67d4f61c
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-26 05:59:20 -0700

    basic try except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+    try: 
+        for item in my_list:
+            if x > idx:
+                print(item, end=" ")
+                idx += 1
+    except IndexError:
+        pass
+    print()
+    return idx

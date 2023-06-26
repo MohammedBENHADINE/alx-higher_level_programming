c0c86fc441d331407412c95ba6454c64f1961d42
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-26 06:01:01 -0700

    fix pycode

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    try: 
+    try:
         for item in my_list:
             if x > idx:
                 print(item, end=" ")

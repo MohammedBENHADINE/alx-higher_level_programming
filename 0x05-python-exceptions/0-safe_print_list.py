4006d582960888d1d361eb51609ec26a545518fd
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-26 06:01:55 -0700

    fix display

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for item in my_list:
             if x > idx:
-                print(item, end=" ")
+                print(item, end="")
                 idx += 1
     except IndexError:
         pass

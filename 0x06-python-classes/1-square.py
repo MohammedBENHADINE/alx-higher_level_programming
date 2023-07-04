021ece9ada719238e397c32eefef25d9a408c95b
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-04 06:03:53 -0700

    class with private att

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task
+Here we define a class Square
+"""
+
+
+class Square:
+    """This is an empty class
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task
+Here we define an empty class
+"""
+
+
+class Square:
+    """This is the Square class
+    Attributes:
+        size (int): size of square
+    """
+    pass

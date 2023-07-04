c6ae7fa33a3293ddb9c9ed804f6d8730363c29ea
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-04 06:15:22 -0700

    class with validation and pub method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task
+Here we define a class Square
+"""
+
+
+class Square:
+    """This is an empty class
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size=0):
+        """Validate if size is an integer
+        It must also be superiour or qual to zero
+        """
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size*4

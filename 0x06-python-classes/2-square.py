78468a5801bf3bd2930ae876084f495f631e3597
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-04 06:07:21 -0700

    class with control on att

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """This is a module to pass a Task
-Here we define an empty class
+Here we define a class Square
 """
 
 
 class Square:
-    """This is the Square class
+    """This is an empty class
     Attributes:
         size (int): size of square
     """
-    pass
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size

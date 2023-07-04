662d727b1cb6fa39aa636000bde459cb1e987384
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-04 01:39:21 -0700

    done wit comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-# This is a module to pass a Task
+"""This is a module to pass a Task
+Here we define an empty class
+"""
+
+
 class Square:
     """This is an empty class
     Nothing to define

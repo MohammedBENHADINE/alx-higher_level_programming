a307a59b3293b4470543abfa3e2f04c81e18b769
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-05 14:37:54 -0700

    fix some code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 class LockedClass:
-    """class that prevents the user from dynamically creating new instance att"""
+    """class that prevents the user from
+    dynamically creating new instance att
+    """
     def __setattr__(self, name, value):
         """Magic class"""
         if hasattr(self, name) or name == 'first_name':
             self.__dict__[name] = value
         else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+            raise AttributeError(
+                    "'LockedClass' object has no attribute '{}'"
+                    .format(name)
+                    )

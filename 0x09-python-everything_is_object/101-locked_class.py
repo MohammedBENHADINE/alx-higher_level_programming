89889e6a4ad471aa40f5b9d76bfd3667ce3b79dc
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-05 14:41:53 -0700

    fix issue

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,7 +8,7 @@ class LockedClass:
     """
     def __setattr__(self, name, value):
         """Magic class"""
-        if not hasattr(self, name) or name == 'first_name':
+        if hasattr(self, name) or name == 'first_name':
             self.__dict__[name] = value
         else:
             raise AttributeError(

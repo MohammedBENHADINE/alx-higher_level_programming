7f44afcdce0bce27c0d8020b6bbce8580c0f7ec8
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-05 03:01:27 -0700

    add att and prop to class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task"""
+
+
+class Rectangle:
+    """Class rectangle with following attributes
+    Attributes:
+        width (int): width of rect
+        height (int): height of rect
+    """
+    def __init__(self, width=0, height=0):
+        """Init magic method"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @heigth.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("heigtht must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value

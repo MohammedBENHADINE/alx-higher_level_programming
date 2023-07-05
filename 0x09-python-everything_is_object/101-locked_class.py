8b56a343ccde00d0c24ab70f0e217eacb75e2cc0
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-05 14:32:45 -0700

    add comments

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 class LockedClass:
+    """class that prevents the user from dynamically creating new instance att"""
     def __setattr__(self, name, value):
+        """Magic class"""
         if hasattr(self, name) or name == 'first_name':
             self.__dict__[name] = value
         else:

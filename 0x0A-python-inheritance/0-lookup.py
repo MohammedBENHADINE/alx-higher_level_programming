488c4cf37c2e6f22f621f0a8709d38b3feb84ac2
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-10 14:00:15 -0700

    fix issue

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This a module's comment"""
 def lookup(obj):
-    """Return a list of attributes of a class"""
-    res = []
-    for attr in dir(obj):
-        res.append(getattr(obj, attr))
-    return res
+    """Return a list of attributes of a class
+    Args:
+        obj: object class
+    """
+    return [i for i in dir(obj)]

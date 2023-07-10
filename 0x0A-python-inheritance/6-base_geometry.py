f55f30e6cd871c041c56a37f0c9eaa9f4fd7ff7d
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-10 14:44:06 -0700

    add method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class module
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")

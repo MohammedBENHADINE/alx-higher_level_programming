f2b648a825ff66958795bd01b1e30591450b3d86
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-03 08:39:09 -0700

    fix if a is Nonde

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """
     res = 0
 
-    if type(a) not in [int, float]:
+    if a is None or (type(a) not in [int, float]):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")

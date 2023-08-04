4fa3327b7da50a49f38a6497113e6dc276cdb6df
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-04 04:25:11 -0700

    change my method

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-    Add two integers
+Module to sum
+add_integer: adds two integers and return
 """
 
 
 def add_integer(a, b=98):
+    """Return sum of a + b
+    Args: a = int/float b = int/float
     """
-    This method checks if both variables are int/float.
-    """
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return (a + b)
+
+    if isinstance(a, (int, float)) is False:
+        raise TypeError('a must be an integer')
+    if isinstance(b, (int, float)) is False:
+        raise TypeError('b must be an integer')
+    return int(a + b)

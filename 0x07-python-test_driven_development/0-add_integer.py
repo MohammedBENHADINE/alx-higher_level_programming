beff8e8592b52b8133bf5905be957f649f3229e8
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-04 04:17:40 -0700

    update func

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,18 +8,12 @@ def add_integer(a, b=98):
     """
     This method checks if both variables are int/float.
     """
-    res = 0
-
     if a is None or (type(a) not in [int, float]):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     if type(a) is float:
-        res += int(a)
-    else:
-        res += a
+        a = int(a)
     if type(b) is float:
-        res += int(b)
-    else:
-        res += b
-    return res
+        b = int(b)
+    return (a + b)

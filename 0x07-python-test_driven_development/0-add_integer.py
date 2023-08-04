57fc866569c5f3301dab5257bafd4378fa7105ef
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-04 04:19:09 -0700

    fix a test on None

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ def add_integer(a, b=98):
     """
     This method checks if both variables are int/float.
     """
-    if a is None or (type(a) not in [int, float]):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")

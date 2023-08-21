8b8d24b74a06cb23fab2eac19507c7935db151f5
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-21 09:32:41 -0700

    database conversions

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- convert database parameters, table and field
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET tf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+

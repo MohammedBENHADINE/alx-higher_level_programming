f686f991b97041e35276f4c48db0f50400e15aa8
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-24 12:05:37 -0700

    create user and database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- create a user and databse
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
-
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';

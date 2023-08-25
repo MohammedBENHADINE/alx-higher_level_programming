22d0c1b84a44578de27adbe449488a377563cf0e
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-24 17:48:42 -0700

    dual left join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all Comdedy shows
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
-JOIN tv_show_genres on tv_shows.id=tv_show_genres.show_id
-JOIN tv_genres on tv_show_genres.genre_id=tv_genres.id
+LEFT JOIN tv_show_genres on tv_shows.id=tv_show_genres.show_id
+LEFT JOIN tv_genres on tv_show_genres.genre_id=tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name;

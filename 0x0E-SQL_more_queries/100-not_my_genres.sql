a5fd5e39844f4da1711351f4756ae9be23d69495
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-24 18:03:02 -0700

    fix order

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,4 +6,5 @@ JOIN tv_show_genres on tv_genres.id=tv_show_genres.genre_id
 JOIN tv_shows on tv_show_genres.show_id=tv_shows.id
 WHERE tv_shows.title="Dexter"
 ORDER BY tv_genres.name ASC
-);
+)
+ORDER BY name;

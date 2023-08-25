1b6e9f502952a9ecc0504ec3e0fba76b11ed288d
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-24 18:33:45 -0700

    two selects only

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,15 +1,11 @@
 -- lists all shows without the genre Comedy in the database
-SELECT DISTINCT tv_shows.title
+SELECT title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.id NOT IN (
-    SELECT show_id
+WHERE id NOT IN (
+    SELECT tv_show_genres.show_id
     FROM tv_show_genres
-    WHERE genre_id = (
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-    )
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
 )
-ORDER BY tv_shows.title ASC;
+ORDER BY title ASC;
+

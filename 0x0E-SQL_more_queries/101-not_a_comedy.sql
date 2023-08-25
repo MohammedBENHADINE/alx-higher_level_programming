3bbc9f0b31e3c45fc43c02e117c09a8f15c968c1
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-08-24 18:13:10 -0700

    no comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,5 +2,5 @@
 SELECT DISTINCT tv_shows.title FROM tv_shows
 LEFT JOIN tv_show_genres on tv_shows.id=tv_show_genres.show_id
 LEFT JOIN tv_genres on tv_show_genres.genre_id=tv_genres.id
-WHERE NOT tv_genres.name="Comedy"
+WHERE tv_genres.name IS NULL OR tv_genres.name != "Comedy"
 ORDER BY tv_shows.title ASC;

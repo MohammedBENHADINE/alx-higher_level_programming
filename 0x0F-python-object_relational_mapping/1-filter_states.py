dc3ea3c1272d1ad01492f9305f386297b4db4aca
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-15 04:15:04 -0700

    fix regexp

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name REGEXP '^N' \
+                 WHERE name REGEXP '^[N]' \
                  ORDER BY id")
     rows = cur.fetchall()
     for row in rows:

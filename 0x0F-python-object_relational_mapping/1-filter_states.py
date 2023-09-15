959527926dc59e7d6bddc4068b3f4b563689b74e
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-15 04:16:36 -0700

    add more clarification to mysql quer

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name REGEXP '^[N]' \
-                 ORDER BY id")
+                 WHERE states.name REGEXP '^[N]' \
+                 ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(f"({row[0]}, '{row[1]}')")

64ed7c5e1e9e6388239f43ffb7697fa7aac8c9d1
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-16 05:45:04 -0700

    change design

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,42 +2,52 @@
 import sys
 import MySQLdb
 
-if len(sys.argv) != 4:
-    print("Usage: python script.py \
-            <mysql_username> <mysql_password> <database_name>")
-    sys.exit(1)
 
-mysql_username, mysql_password, database_name = \
+def list_states_with_n(mysql_username, mysql_password, database_name):
+    try:
+        # Connect to the MySQL server
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
+
+        # Create a cursor object to interact with the database
+        cursor = connection.cursor()
+
+        # Execute the SQL query to retrieve states starting with 'N'
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+        cursor.execute(query)
+
+        # Fetch and return the results
+        results = cursor.fetchall()
+        return results
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        return None
+
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py \
+                <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    mysql_username, mysql_password, database_name = \
         sys.argv[1], sys.argv[2], sys.argv[3]
 
-try:
-    # Connect to the MySQL server
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
+    results = list_states_with_n(mysql_username, mysql_password, database_name)
 
-    # Create a cursor object to interact with the database
-    cursor = connection.cursor()
-
-    # Execute the SQL query to retrieve states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
-    cursor.execute(query)
-
-    # Fetch and display the results
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-except MySQLdb.Error as e:
-    print("MySQL Error:", e)
-
-finally:
-    # Close the cursor and connection
-    if cursor:
-        cursor.close()
-    if connection:
-        connection.close()
+    if results:
+        for row in results:
+            print(row)

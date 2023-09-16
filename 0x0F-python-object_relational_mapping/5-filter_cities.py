7939e741a3b9005858448b53785b34f7468a3eb8
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-16 07:41:22 -0700

    Revert "free code injectionj"
    This reverts commit 9ad277d79dec7a0b2d2c6d9b585ebc8805a03ec4.

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -22,9 +22,9 @@ def list_cities_by_state(mysql_username, mysql_password, database_name, state):
         # Execute the SQL query to retrieve states starting with 'N'
         query = "SELECT cities.id, cities.name \
                 FROM cities JOIN states ON cities.state_id=states.id \
-                WHERE states.name={} \
-                ORDER BY cities.id ASC;".format(state)
-        cursor.execute(query)
+                WHERE states.name=%s \
+                ORDER BY cities.id ASC;"
+        cursor.execute(query, (state,))
 
         # Fetch and return the results
         results = cursor.fetchall()

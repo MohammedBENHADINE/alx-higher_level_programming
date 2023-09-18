e57246919b476137415d2947266a7756514eb070
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-18 08:22:27 -0700

    New isntance add and commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,3 +14,6 @@ class State(Base):
     id = Column(Integer, autoincrement=True, nullable=False,
                 unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name

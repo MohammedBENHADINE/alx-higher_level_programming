44631a9d033edbb4ccda0c9594284c6f6752fffc
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-09-18 17:03:41 -0700

    change

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""lists all State objects that contain the letter a from the database
-"""
-import sys
+"""List all states"""
+from sys import argv
 from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-    Base.metadata.create_all(engine)
     session = Session()
-    states = session.query(State).join(City).order_by(State.id, City.id)
-    for state in states:
+
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-    session.close()
+            print("\t{}: {}".format(city.id, city.name))

d3298918bc957e310f7a615a096fb38e8beb837d
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-10-01 12:35:29 -0700

    lines to 3 only

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# get response from server and display data length
-
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-url="$1"
-res_file=$(mktemp)
-curl -s -o "$res_file" "$url"
-
-echo $(cat "$res_file" | wc -c)
-rm -f "$res_file"
+curl -s -o tmp "$1"
+echo $(cat tmp | wc -c)
